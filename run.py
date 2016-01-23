800245c4f39d1cdf1e9724f9f80fa35a761f1fdc
Soloviev Denis <ombschervister@gmail.com>
2016-01-24 01:49:01 +0500

    run pyqt widget with png and svg images

---
+++ run.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+import sys
+from PyQt4 import QtGui
+import rc
+
+
+class Widget(QtGui.QWidget):
+    def __init__(self):
+        QtGui.QWidget.__init__(self)
+
+        png_label = QtGui.QLabel()
+        svg_label = QtGui.QLabel()
+
+        v_layout = QtGui.QVBoxLayout(self)
+        v_layout.addWidget(QtGui.QLabel("png:"))
+        v_layout.addWidget(png_label)
+        v_layout.addWidget(QtGui.QLabel("svg:"))
+        v_layout.addWidget(svg_label)
+
+        png_label.setPixmap(QtGui.QPixmap(":/example1.png").scaled(200, 200))
+        svg_label.setPixmap(QtGui.QPixmap(":/example2.svg").scaled(200, 200))
+
+
+app = QtGui.QApplication(sys.argv)
+w = Widget()
+w.show()
+sys.exit(app.exec_())
